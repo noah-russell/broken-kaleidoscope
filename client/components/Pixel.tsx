ff72529256456be80031fe72f179ae159f58e2c9
arianne-hallgarth <143327404+arianne-hallgarth@users.noreply.github.com>
2023-10-31 10:54:51 +1300

    useColor

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
+
 export function Pixel() {
+  const [color, setColor] = useState('blue')
+
+  function changeColor() {
+    setColor('red')
+  }
   return (
     <div
       style={{
         fontFamily: 'Times New Roman',
         height: '15px',
         width: '15px',
-        backgroundColor: 'blue',
+        backgroundColor: color,
       }}
     ></div>
   )
