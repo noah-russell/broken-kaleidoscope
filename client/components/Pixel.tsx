6bcad0c44738c15cf896ec2550652eb64ff4b2c2
anthonymartin36 <anthony.martin36@gmail.com>
2023-10-31 12:41:49 +1300

    Up to step 4

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -1,6 +1,18 @@
-
+import {useState} from 'react' 
 
 function Pixel(){
-    return  (<div style={{fontFamily: 'Times New Roman', width:'100px', height:'100px', background: '#24C8F0'}}>  </div>)
+
+    const [color, setColor] = useState('#24C8F0')
+    console.log(color)
+    return  (
+        <div 
+            style={{
+                fontFamily: 'Times New Roman', 
+                background: color,
+                width:'100px', 
+                height:'100px', 
+                }}> 
+        </div>
+    )
 }
 export default Pixel
