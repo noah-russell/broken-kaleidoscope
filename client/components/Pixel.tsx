3cf86b9a3184263f2774e46ef4514ee4d2d85b8f
Tristan Bulmer <63182722+Hoffee-Toffee@users.noreply.github.com>
2023-10-31 09:56:09 +1300

    Pixel basic styling

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -1,5 +1,14 @@
 function Pixel() {
-  return <p>some text</p>
+  return (
+    <div
+      style={{
+        fontFamily: 'Times New Roman',
+        height: '60px',
+        width: '60px',
+        backgroundColor: '#c9510b',
+      }}
+    ></div>
+  )
 }
 
 export default Pixel
