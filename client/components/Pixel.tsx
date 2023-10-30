bf7b5cea96eff37a105adc97782b4e867f6889e5
Williford Abunas <142576037+williford-abunas@users.noreply.github.com>
2023-10-31 11:00:08 +1300

    done step 5

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
+const getRandomColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000)
+    .toString(16)
+    .padStart(6, '0')}`
 
 function Pixel() {
-  const [colour, setColor] = useState('blue')
-
+  const [colour, setColor] = useState(getRandomColor)
   return (
     <div
       style={{ backgroundColor: colour, height: '100px', width: '100px' }}
