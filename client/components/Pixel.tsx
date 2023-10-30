1aa618757044a1e916607679fd58ba21c29df2f4
Blue Pilkinton-Ching <49179161+Blue-Pilkinton-Ching@users.noreply.github.com>
2023-10-31 11:01:21 +1300

    Completed step 5

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -1,7 +1,12 @@
 import { useState } from 'react'
 
 function Pixel() {
-  const [color, setColor] = useState('yellow')
+  const getRandomColor = () =>
+    `#${Math.floor(Math.random() * 0x1000000)
+      .toString(16)
+      .padStart(6, '0')}`
+
+  const [color, setColor] = useState(getRandomColor)
 
   return (
     <div
