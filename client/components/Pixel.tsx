87a2be9000d1e7ec927e2a798b0025ed0d38cea8
danielkingstonweb <87955791+danielkingstonweb@users.noreply.github.com>
2023-10-31 11:03:14 +1300

    random colors posting

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -1,8 +1,15 @@
+import { useState } from 'react'
+
 function Pixel() {
+  const getRandomColor = () =>
+    `#${Math.floor(Math.random() * 0x1000000)
+      .toString(16)
+      .padStart(6, '0')}`
+  const [color, changeColor] = useState(getRandomColor())
   return (
     <div
       style={{
-        background: 'cyan',
+        background: color,
         height: '30px',
         width: '30px',
       }}
