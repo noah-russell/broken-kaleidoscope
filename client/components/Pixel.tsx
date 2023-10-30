a1ad9029223ed04c9c51f045222beb255b3c40a0
keeley <142860097+keeley-owen@users.noreply.github.com>
2023-10-31 11:02:00 +1300

    change color

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -1,7 +1,18 @@
-function Pixel(){
-return (
-  <div style={{backgroundColor: 'blue', width: '30px', height: '30px', margin: '1px'}}/>
-)
+import { useState } from 'react'
+
+function Pixel() {
+  const [color, setColor] = useState('salmon')
+
+  return (
+    <div
+      style={{
+        backgroundColor: color,
+        width: '30px',
+        height: '30px',
+        margin: '1px',
+      }}
+    />
+  )
 }
 
 export default Pixel
