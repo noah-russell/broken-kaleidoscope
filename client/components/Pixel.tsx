32c1343b70b2a9c57b981189c70dda95eb311138
Rose-Northey <132635605+Rose-Northey@users.noreply.github.com>
2023-10-31 14:21:05 +1300

    added randomise colour

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -1,7 +1,21 @@
 import { useState } from 'react'
 
 function Pixel() {
-  const [color, setColor] = useState('salmon')
+  function randomColor(){
+    return (
+      Math.floor(Math.random()*9+1)+ 
+      (Math.floor(Math.random()*9+1))*10 + 
+      (Math.floor(Math.random()*9+1))*100 +
+      (Math.floor(Math.random()*9+1))*1000
+    )
+  }
+
+  console.log( Math.floor(Math.random()*9+1)*10)
+  const threeNum = randomColor()
+
+  const [color, setColor] = useState('#00'+threeNum)
+
+
 
   return (
     <div
