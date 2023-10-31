b5cbb9f0677362b7c8ac957691c4fc941f201b72
Rose-Northey <132635605+Rose-Northey@users.noreply.github.com>
2023-10-31 15:28:26 +1300

    add drageneter

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -6,24 +6,27 @@ function Pixel() {
       .toString(16)
       .padStart(3, '0')}`
 
-
   const [colour, setColour] = useState(getRandomColour)
 
   const handleClick = (event: UIEvent) => {
     setColour(getRandomColour)
   }
 
-
+  const handleDragEnter = (event: UIEvent) => {
+    setColour('pink')
+  }
 
   return (
     <div
       style={{
-        backgroundColor: colour ,
+        backgroundColor: colour,
         width: '30px',
         height: '30px',
-        margin: '1px',
+        // margin: '1px',
+        padding: '2px',
       }}
       onClick={handleClick}
+      onDragEnter={handleDragEnter}
     />
   )
 }
