a73e45bf52a822f41a5e710bbefed0e7c32b572e
Matthew Ranford <matt.ranford16@gmail.com>
2023-10-31 14:03:49 +1300

    Onclick function added

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -5,10 +5,15 @@ function Pixel() {
     `#${Math.floor(Math.random() * 0x1000000)
       .toString(16)
       .padStart(6, '0')}`
+
   const [colour, setColour] = useState(getRandomColor())
 
+  const handleClick = (event: UIEvent) => {
+    setColour('black')
+  }
   return (
     <div
+      onClick={handleClick}
       style={{ backgroundColor: colour, height: '50px', width: '50px' }}
     ></div>
   )
