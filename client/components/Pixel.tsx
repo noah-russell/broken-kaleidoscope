81a5db6a7e42974ed1236225fec5ead4cec4b6e2
Rose-Northey <132635605+Rose-Northey@users.noreply.github.com>
2023-10-31 15:05:39 +1300

    hexidecimal input

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -1,26 +1,20 @@
 import { useState } from 'react'
 
 function Pixel() {
-  function randomColor(){
-    return (
-      Math.floor(Math.random()*9+1)+ 
-      (Math.floor(Math.random()*9+1))*10 + 
-      (Math.floor(Math.random()*9+1))*100 +
-      (Math.floor(Math.random()*9+1))*1000
-    )
-  }
-
-  console.log( Math.floor(Math.random()*9+1)*10)
-  const threeNum = randomColor()
-
-  const [color, setColor] = useState('#00'+threeNum)
+  const getRandomColor = () =>
+    `#${Math.floor(Math.random() * 0x1000)
+      .toString(16)
+      .padStart(3, '0')}`
+  console.log(getRandomColor)
 
 
+
+  const [color, setColor] = useState(getRandomColor)
 
   return (
     <div
       style={{
-        backgroundColor: color,
+        backgroundColor: color ,
         width: '30px',
         height: '30px',
         margin: '1px',
