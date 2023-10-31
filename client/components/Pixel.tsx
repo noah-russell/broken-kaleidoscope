e589250a5c2f81ce1b427bc6bab9a9ee610b933c
Matthew Ranford <matt.ranford16@gmail.com>
2023-10-31 13:39:36 +1300

    Rendering random colours on page

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -1,7 +1,15 @@
+import { useState } from 'react'
+
 function Pixel() {
+  const getRandomColor = () =>
+    `#${Math.floor(Math.random() * 0x1000000)
+      .toString(16)
+      .padStart(6, '0')}`
+  const [colour, setColour] = useState(getRandomColor())
+
   return (
     <div
-      style={{ backgroundColor: 'green', height: '50px', width: '50px' }}
+      style={{ backgroundColor: colour, height: '50px', width: '50px' }}
     ></div>
   )
 }
