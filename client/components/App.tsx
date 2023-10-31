c9e44c8247d9bc78e88814bac43779ac2dae4c5c
IggyNash <143356841+IggyNash@users.noreply.github.com>
2023-10-31 14:57:24 +1300

    step 8 complete

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,12 +1,18 @@
 import Pixel from './pixel'
 
 function App() {
-  const pixArr = Array(5000).fill(<Pixel />)
-  return (
-    <div>
-    {pixArr}
-    </div>
-  )
+  const pixArr = Array(100000).fill(<Pixel />)
+  return <div>{pixArr}</div>
+
+  // const pixArr = []
+  // for (let x = 0; x < 10000; x++) {
+  //   pixArr.push(
+  //     <div>
+  //       <Pixel />
+  //     </div>
+  //   )
+  // }
+  // return Array.from(pixArr)
 }
 
 export default App
